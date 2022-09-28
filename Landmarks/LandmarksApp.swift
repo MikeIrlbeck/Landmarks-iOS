e1f902bd03a8542a772c53cca07d48fb106c722d
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2022-09-27 20:13:54 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Michael Irlbeck on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
